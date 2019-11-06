c8916bdacbdc1a29380d757b65d33f0fcee3a488
Alessandra Longaretti <tauan@Tauans-iMac.fios-router.home>
2019-11-06 14:31:29 -0500

    completed Menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,46 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+function createMenu (a,b,c,d,e,f) {
+  const menu = document.createElement('div');
+  const menuUl = document.createElement('ul');
+  const menuOne = document.createElement('li');
+  const menuTwo = document.createElement('li');
+  const menuThree = document.createElement('li');
+  const menuFour = document.createElement('li');
+  const menuFive = document.createElement('li');
+  const menuSix = document.createElement('li');
+
+  menu.appendChild(menuUl);
+
+  menuUl.appendChild(menuOne);
+  menuUl.appendChild(menuTwo);
+  menuUl.appendChild(menuThree);
+  menuUl.appendChild(menuFour);
+  menuUl.appendChild(menuFive);
+  menuUl.appendChild(menuSix);
+
+  menu.classList.add('menu');
+
+
+
+  const menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', () => {
+  
+  const menuDiv = document.querySelector('.menu')
+  menuDiv.classList.toggle('menu--open');
+  });
+
+  menuOne.textContent = a;
+  menuTwo.textContent = b;
+  menuThree.textContent = c;
+  menuFour.textContent = d;
+  menuFive.textContent = e;
+  menuSix.textContent = f;
+
+  return menu;
+}
+
+const container2 = document.querySelector('.header');
+container2.appendChild(createMenu(menuItems));
